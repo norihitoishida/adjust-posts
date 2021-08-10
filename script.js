fc934fa531dc06745aafcf77b4ec70bf7e535d67
norihitoishida <norihitoishida@gmail.com>
2021-08-10 18:10:37 +0900

    update

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 function cleanup(otayori) {
+    // String型にする
+    otayori = String(otayori);
 
+    // 半角・全角スペースと改行を「。」にする
+    otayori = otayori.replace(/ /g, "。");
+    otayori = otayori.replace(/　/g, "。");
+    otayori = otayori.replace(/\r?\n/g, "。");
+
+    // 「。」が2個以上連続してる所を「。」1個にする
+    otayori = otayori.replace(/。{2,}/g, "。");
+
+    // 「。」を「。改行改行」にする
+    otayori = otayori.replace(/。/g, "。\n\n");
     
     return otayori;
-};
+}
 
 function adjust() {
     let otayori = document.getElementById("otayori_raw").value;
     otayori = cleanup(otayori);
     document.getElementById("otayori_adjusted").value = otayori;
-};
+}
 
 function zoomin(){
     fontSize = Math.min(fontSize+5, 100);
